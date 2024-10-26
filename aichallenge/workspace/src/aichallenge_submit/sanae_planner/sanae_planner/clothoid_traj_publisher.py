73c206280ac221ba1d149dbe8af58c37ea9d652b
Ikoma So <ikoma-so@g.ecc.u-tokyo.ac.jp>
2024-10-26 15:21:08 +0900

    clothoid経路

---
+++ aichallenge/workspace/src/aichallenge_submit/sanae_planner/sanae_planner/clothoid_traj_publisher.py
@@ -0,0 +1,79 @@
+import rclpy
+from rclpy.node import Node
+
+from sklearn.neighbors import NearestNeighbors
+from autoware_auto_planning_msgs.msg import Trajectory, TrajectoryPoint
+from tf_transformations import quaternion_from_euler
+
+from nav_msgs.msg import Odometry
+
+import numpy as np
+
+class ClothoidTrajPublisher(Node):
+
+  def __init__(self):
+    super().__init__('clothoid_traj_publisher')
+    self.declare_parameter('traj_file', '/aichallenge/workspace/src/aichallenge_submit/sanae_planner/trajectory/traj.csv')
+    self.declare_parameter('wheel_base', 1.087)
+    self.declare_parameter('speed_km_h', 10.0)
+        
+    self.traj_pub = self.create_publisher(Trajectory, '/planning/scenario_planning/trajectory', 10)
+    self.odom_sub = self.create_subscription(Odometry, 'input/odom', self.odom_callback, 10)
+
+    self.traj = np.loadtxt(self.get_parameter('traj_file').value, delimiter=',')
+    self.x = self.traj[:, 0]
+    self.y = self.traj[:, 1]
+    self.yaw = self.traj[:, 2]
+    self.curvature = self.traj[:, 3]
+    
+    self.wheel_base = self.get_parameter('wheel_base').value
+    self.speed_mps = self.get_parameter('speed_km_h').value / 3.6
+    
+    self.nn = NearestNeighbors(n_neighbors=1, algorithm='ball_tree').fit(np.array([self.x, self.y]).T)
+    self.nearest_idx = 0
+
+    self.odom = None
+    
+    while self.odom is None:
+      self.get_logger().info('Waiting for odometry...')
+      rclpy.spin_once(self)
+      
+    self.create_timer(0.1, self.publish_traj)
+      
+      
+  def odom_callback(self, msg):
+    self.odom = msg
+    _, idx = self.nn.kneighbors(np.array([[self.odom.pose.pose.position.x, self.odom.pose.pose.position.y]]))
+    self.nearest_idx = idx[0][0]
+    
+  def publish_traj(self):
+    self.speed_mps = self.get_parameter('speed_km_h').value / 3.6
+    traj_msg = Trajectory()
+    traj_msg.header.stamp = self.get_clock().now().to_msg()
+    traj_msg.header.frame_id = 'map'
+    
+    for i in range(0, len(self.x)):
+      traj_point = TrajectoryPoint()
+      idx = (i + self.nearest_idx) % len(self.x)
+      traj_point.pose.position.x = self.x[i]
+      traj_point.pose.position.y = self.y[i]
+      traj_point.pose.position.z = 0.0
+      q = quaternion_from_euler(0, 0, self.yaw[i])
+      traj_point.pose.orientation.x = q[0]
+      traj_point.pose.orientation.y = q[1]
+      traj_point.pose.orientation.z = q[2]
+      traj_point.pose.orientation.w = q[3]
+      traj_point.longitudinal_velocity_mps = self.speed_mps
+      traj_point.front_wheel_angle_rad = np.arctan(self.curvature[i] * self.wheel_base)
+      traj_msg.points.append(traj_point)
+      
+    self.traj_pub.publish(traj_msg)
+    
+def main(args=None):
+  rclpy.init(args=args)
+  node = ClothoidTrajPublisher()
+  rclpy.spin(node)
+  rclpy.shutdown()
+  
+if __name__ == '__main__':
+  main()
--- aichallenge/workspace/src/aichallenge_submit/sanae_planner/setup.py
+++ aichallenge/workspace/src/aichallenge_submit/sanae_planner/setup.py
@@ -1,16 +1,28 @@
 from setuptools import find_packages, setup
 
+import os 
+from glob import glob
+
 package_name = 'sanae_planner'
+
+data_files = []
+data_files.append(("share/ament_index/resource_index/packages", ["resource/" + package_name]))
+data_files.append(("share/" + package_name, ["package.xml"]))
+
+def package_files(directory, data_files):
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            data_files.append(("share/" + package_name + "/" + path, glob(path + "/**/*.*", recursive=True)))
+    return data_files
+
+data_files = package_files('trajectory/', data_files)
+
 
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
-    data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-    ],
+    data_files=data_files,
     install_requires=['setuptools'],
     zip_safe=True,
     maintainer='ikoma',
@@ -20,7 +32,8 @@ setup(
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-            'trajectory_optimizer = sanae_planner.trajectory_optimizer:main'
+            'trajectory_optimizer = sanae_planner.trajectory_optimizer:main',
+            'clothoid_traj_publisher = sanae_planner.clothoid_traj_publisher:main',
         ],
     },
 )
