1564eb9f50e570d42ad27ef6d02bea4727873abe
juno-cosmos <junoda1017@g.ecc.u-tokyo.ac.jp>
2024-11-03 20:05:19 +0900

    マルチキャスト有効化とバッファサイズ変更用bashスクリプト

---
+++ aichallenge/increase_network_buffers.sh
@@ -0,0 +1,4 @@
+# configure the network buffers to the maximum value
+# executed when print "ros2: failed to increase socket receive buffer size to at least 10485760 bytes, current is 425984 bytes"
+sudo ip link set multicast on lo
+sudo sysctl -w net.core.rmem_max=2147483647 >/dev/null
