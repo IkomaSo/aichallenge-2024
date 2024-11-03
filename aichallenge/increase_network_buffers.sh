2d4db0fd8c12051d6171caa983a703ba8c924b20
juno-cosmos <junoda1017@g.ecc.u-tokyo.ac.jp>
2024-11-03 20:09:01 +0900

    権限追加

--- aichallenge/increase_network_buffers.sh
+++ aichallenge/increase_network_buffers.sh
@@ -1,3 +1,4 @@
+# マルチキャスト有効化とバッファサイズ変更用bashスクリプト
 # configure the network buffers to the maximum value
 # executed when print "ros2: failed to increase socket receive buffer size to at least 10485760 bytes, current is 425984 bytes"
 sudo ip link set multicast on lo
