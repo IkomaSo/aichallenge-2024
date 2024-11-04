675dc2dfa01151228633619a9ea5abee7f2da4f3
Ikoma So <ikoma-so@g.ecc.u-tokyo.ac.jp>
2024-11-05 00:25:49 +0900

    起動時の引数でパラメータと制御器・経路を変更できるように

--- aichallenge/run_autoware.bash
+++ aichallenge/run_autoware.bash
@@ -2,6 +2,8 @@
 
 mode=${1}
 
+trajectory=${2}
+
 case "${mode}" in
 "awsim")
     opts="simulation:=true use_sim_time:=true run_rviz:=true wheel_base:=1.087"
@@ -18,9 +20,42 @@ case "${mode}" in
     ;;
 esac
 
+trajectory=${2}
+
+traj_path="/aichallenge/workspace/src/aichallenge_submit/sanae_planner/trajectory/${trajectory}.csv"
+
+if [ ! -e ${traj_path} ]; then
+    echo "traj_path: ${traj_path}"
+    echo "invalid trajectory"
+    exit 1
+fi
+
+traj_opts="trajectory:=${trajectory}"
+
+controller=${3}
+preset=${4}
+
+if [ "${controller}" = "smc" ]; then
+    opts="${opts} controller:=smc"
+elif [ "${controller}" = "pid" ]; then
+    opts="${opts} controller:=pid"
+else
+    echo "invalid controller (use 'smc' or 'pid')"
+    exit 1
+fi
+
+config_path="/aichallenge/workspace/src/aichallenge_submit/sanae_control/config/${preset}.yaml"
+if [ ! -e ${config_path} ]; then
+    echo "config_path: ${config_path}"
+    echo "invalid preset"
+    exit 1
+fi
+
+preset_opts="controller_param:=${preset}"
+
 # shellcheck disable=SC1091
 source /aichallenge/workspace/install/setup.bash
 sudo ip link set multicast on lo
 
 # shellcheck disable=SC2086
-ros2 launch aichallenge_system_launch aichallenge_system.launch.xml ${opts}
+ros2 launch aichallenge_system_launch aichallenge_system.launch.xml ${opts} ${traj_opts} ${preset_opts}
