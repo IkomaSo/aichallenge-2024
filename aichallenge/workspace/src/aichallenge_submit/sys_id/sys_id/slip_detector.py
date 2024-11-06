b7cf5a816d0debd1f6ebbd80a5cce1b4d080bd0c
Ikoma So <ikoma-so@g.ecc.u-tokyo.ac.jp>
2024-11-07 01:02:01 +0900

    wheel_baseの推定

--- aichallenge/workspace/src/aichallenge_submit/sys_id/sys_id/slip_detector.py
+++ aichallenge/workspace/src/aichallenge_submit/sys_id/sys_id/slip_detector.py
@@ -4,7 +4,7 @@ from autoware_auto_vehicle_msgs.msg import VelocityReport, SteeringReport
 from sensor_msgs.msg import Imu
 from std_msgs.msg import Float32
 
-from math import atan2
+from math import atan2, tan
 
 class SlipDetector(Node):
   def __init__(self):
@@ -18,9 +18,11 @@ class SlipDetector(Node):
     self.imu_data = None
     self.steer_report_sub = self.create_subscription(SteeringReport, '/vehicle/status/steering_status', self.steer_report_callback, 10)
     self.steer_report = None
+    self.wheel_base_est_buf = []
 
     self.heading_angle_pub = self.create_publisher(Float32, '/slip_detector/heading_angle', 10)
     self.slip_angle_pub = self.create_publisher(Float32, '/slip_detector/slip_angle', 10)
+    self.wheel_base_pub = self.create_publisher(Float32, '/slip_detector/wheel_base', 10)
     
   def steer_report_callback(self, msg):
     self.steer_report = msg
@@ -32,9 +34,16 @@ class SlipDetector(Node):
     vel = msg.longitudinal_velocity
     if vel <= 0.0 or self.steer_report is None or self.imu_data is None:
       return
-    # omega = msg.heading_rate
-    omega = self.imu_data.angular_velocity.z
+    omega = msg.heading_rate
+    # omega = self.imu_data.angular_velocity.z
     heading_angle = atan2(omega * self.wheel_base, vel)
+    
+    wheel_base_est = tan(self.steer_report.steering_tire_angle) * vel / omega
+    self.wheel_base_est_buf.append(wheel_base_est)
+    if len(self.wheel_base_est_buf) > 100:
+      self.wheel_base_est_buf.pop(0)
+    wheel_base_est = sum(self.wheel_base_est_buf) / len(self.wheel_base_est_buf)
+    self.wheel_base_pub.publish(Float32(data=wheel_base_est))
 
     slip_angle = self.steer_report.steering_tire_angle - heading_angle
     
